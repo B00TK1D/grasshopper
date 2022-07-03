a7611738131cf68764028afc3648d61e5241c592
B00TK1D <stearns.josiah@gmail.com>
2022-07-03 03:31:40 -0400

    testing python

--- exfil.py
+++ exfil.py
@@ -1,20 +1,27 @@
-from __future__ import print_function
-from scapy.all import ( Dot11,
-                        Dot11Beacon,
-                        Dot11Elt,
-                        RadioTap,
-                        sendp,
-                        hexdump)
+from scapy.all import Dot11,Dot11Beacon,Dot11Elt,RadioTap,sendp,hexdump
 
-SSID = 'Test SSID' 
-iface = 'wlp2s0'   
-sender = 'ac:cb:12:ad:58:27'
+netSSID = 'testSSID'       #Network name here
+iface = 'en0'         #Interface name here
 
 dot11 = Dot11(type=0, subtype=8, addr1='ff:ff:ff:ff:ff:ff',
-addr2=sender, addr3=sender)
-beacon = Dot11Beacon()
-essid = Dot11Elt(ID='SSID',info=SSID, len=len(SSID))
+addr2='22:22:22:22:22:22', addr3='33:33:33:33:33:33')
+beacon = Dot11Beacon(cap='ESS+privacy')
+essid = Dot11Elt(ID='SSID',info=netSSID, len=len(netSSID))
+rsn = Dot11Elt(ID='RSNinfo', info=(
+'\x01\x00'                 #RSN Version 1
+'\x00\x0f\xac\x02'         #Group Cipher Suite : 00-0f-ac TKIP
+'\x02\x00'                 #2 Pairwise Cipher Suites (next two lines)
+'\x00\x0f\xac\x04'         #AES Cipher
+'\x00\x0f\xac\x02'         #TKIP Cipher
+'\x01\x00'                 #1 Authentication Key Managment Suite (line below)
+'\x00\x0f\xac\x02'         #Pre-Shared Key
+'\x00\x00'))               #RSN Capabilities (no extra capabilities)
 
-frame = RadioTap()/dot11/beacon/essid
+frame = RadioTap()/dot11/beacon/essid/rsn
+
+frame.show()
+print("\nHexdump of frame:")
+hexdump(frame)
+#raw_input("\nPress enter to start\n")
 
 sendp(frame, iface=iface, inter=0.100, loop=1)
